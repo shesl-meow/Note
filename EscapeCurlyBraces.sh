bb95dc022c6c04310e7caf886edb845c466bd399
shesl-meow <1184249450@qq.com>
2019-02-07 19:48:29 +0800

    [add] Escape curly braces script

---
+++ EscapeCurlyBraces.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+if [ $1 = '--help' ] || [ $1 = '-h' ]; then
+	echo "This is a bash script used to escape\n  double curly braces for gitbook using:\n" \
+		"  - Example1: '{{' ==> '{% raw %}{{{% endraw %}'\n" \
+		"  - Example2: '{%' ==> '{% raw %}{%{% endraw %}'\n"
+	echo "Parameter:\n" \
+		"  p1: filename or foldername.\n"
+	exit 0
+fi
+
+escape_file () {
+	# Parameter:
+	#	p1: the escape target filename
+	sed 's/{{/__AA__/g; s/}}/__BB__/g; s/{%/__AX__/g; s/%}/__BX__/g' $1 > tmp;
+	sed 's/__AA__/{% raw %}{{{% endraw %}/g; s/__BB__/{% raw %}}}{% endraw %}/g;
+		s/__AX__/{% raw %}{%{% endraw %}/g; s/__BX__/{% raw %}%}{% endraw %}/g' tmp > $1
+	rm tmp;
+	echo "Escape all pattern on file[$1]"
+}
+
+if [ -f $1 ]; then
+	escape_file $1;
+elif [ -d $1 ]; then
+	find $1 -type f -name -not "*EscapeCurlyBraces.sh" | while read f;
+	do
+		escape_file $f;
+	done
+else
+	echo "Illegal parameter"
+	exit 1
+fi
