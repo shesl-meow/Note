dce1439e4043934518b2d9b13530b658ac478d8c
shesl-meow@Manjaro <shesl-meow@qq.com>
2019-02-27 00:57:09 +0800

    [fix] double dollar problem & PushHook

--- PushHook.sh
+++ PushHook.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 
-chmod +x *.sh
-
+# TODO: change directory to the shell script dir
+cd `$(dirname "$0")`
 # TODO: reset all change back
 git checkout HEAD .
-
 # TODO: pull the newest version.
 git pull
 
+chmod +x $SELFPATH/*.sh
 ./CheckReadme.sh .
 ./EscapeCurlyBraces.sh .
 
+gitbook install
 gitbook build
